c82b10b115edb87da89d88f1eda03190d2fd8f54
= <martoncsab@gmail.com>
2016-04-14 10:07:39 +0200

    move the session factory reaching to the abstract level

--- src/main/java/com/smdental/dao/TreatmentDaoImpl.java
+++ src/main/java/com/smdental/dao/TreatmentDaoImpl.java
@@ -3,9 +3,6 @@ package com.smdental.dao;
 import com.smdental.model.Treatment;
 import org.hibernate.Criteria;
 import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,9 +11,6 @@ import java.util.List;
 @Repository
 @Transactional
 public class TreatmentDaoImpl extends AbstractDao<Long, Treatment> implements TreatmentDao {
-
-	@Autowired
-	private SessionFactory sessionFactory;
 
 	@Override
 	public void insertTreatment(Treatment treatment)
@@ -43,8 +37,6 @@ public class TreatmentDaoImpl extends AbstractDao<Long, Treatment> implements Tr
 		Query query = getSession().createSQLQuery("delete from Employee where treatment.treatmentid = :id");
 		query.setLong("id", treatmentId);
 		query.executeUpdate();
-		Session session = sessionFactory.getCurrentSession();
-		session.delete(findById(treatmentId));
 	}
 
 }
